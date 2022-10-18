b19c19d6fb3723cb6af5365a0c8756aa21bcfc06
Rohan Thacker <rohan.thacker@yourstory.com>
2022-10-18 18:14:42 +0530

    Added: Initial User onboarding flow

--- lib/pages.dart
+++ lib/pages.dart
@@ -1,2 +1,4 @@
 export "src/pages/home_page.dart";
+export "src/pages/main_menu.dart";
+export "src/pages/login_page.dart";
 export "src/pages/profile_selection.dart";
---
+++ lib/src/pages/main_menu.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MainMenu extends ConsumerWidget {
+  final Map<String,dynamic> user;
+  const MainMenu(this.user,{super.key});
+
+
+  _navigateToScreen(context, path) {
+    Navigator.of(context).push(path);
+  }
+
+  Widget _buildMainMenuForUser(context) {
+    print("Building Menu for profile type");
+    switch (user['profileType']) {
+      case 1:
+        return Text("Care Giver Menu");
+      case 2:
+        return Text("Doctor Menu");
+      default:
+        return Column(
+          children: [
+            ElevatedButton(onPressed: () => _navigateToScreen(context, '/reverse_spell'), child: Text('ToDo'),),
+            ElevatedButton(onPressed: () => _navigateToScreen(context, '/reverse_spell'), child: Text('Word Search'),),
+            ElevatedButton(onPressed: () => _navigateToScreen(context, '/reverse_spell'), child: Text('Reverse Spell'),),
+            ElevatedButton(onPressed: () => _navigateToScreen(context, '/reverse_spell'), child: Text('Picture Quiz'),),
+          ],
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return _buildMainMenuForUser(context);
+  }
+}
