ea86f505cdf33f8f5b5035daec31c48d5d241983
Varuni <92420645+varuni7@users.noreply.github.com>
2022-12-03 13:39:24 +0530

    Feature/reverse spell1 (#16)
    * making rever_spell dunamic
    
    * fixed signup page
    
    * Feature/reverse spell1 (#14) (#15)
    
    * making rever_spell dynamic
    
    * fixed signup page
    
    * fixed the start button on reverse spell

--- lib/src/pages/signup_form.dart
+++ lib/src/pages/signup_form.dart
@@ -365,7 +365,11 @@ class _QueryFormPageState extends State<QueryFormPage> {
                           'fav_food': fav_food,
                           'allergy': allergies,
                         });
-                        Navigator.of(context).pushNamed(PatientScreen.path);
+
+                        await Navigator.of(context)
+                            .pushNamed(PatientScreen.path);
+
+                
                       }
 
                       // Only if the input form is valid (the user has entered text)
